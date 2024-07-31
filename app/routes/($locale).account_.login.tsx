7b8232ea91873bca91b5a0fa72fa43815016cd74
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-07-31 10:06:14 +1000

    Updated login route
    - Added a form with a post action to login with auth0

--- app/routes/($locale).account_.login.tsx
+++ app/routes/($locale).account_.login.tsx
@@ -1,5 +1,17 @@
+import {Form} from '@remix-run/react';
 import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import {useAuthenticator} from '~/utils/auth.server';
 
 export async function loader({request, context}: LoaderFunctionArgs) {
-  return context.customerAccount.login();
+  return await useAuthenticator(request, context).isAuthenticated(request, {
+    successRedirect: '/account/authorize/multipass',
+  });
+}
+
+export default function Login() {
+  return (
+    <Form action="/account/authorize" method="POST">
+      <button>Login with Auth0</button>
+    </Form>
+  );
 }
