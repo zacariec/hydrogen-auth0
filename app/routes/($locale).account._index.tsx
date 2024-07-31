b22677dd52a154cf6f25d0b8ca0335a12a08b7a3
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-07-31 10:02:57 +1000

    Updated account route redirects

--- app/routes/($locale).account._index.tsx
+++ app/routes/($locale).account._index.tsx
@@ -1,5 +1,13 @@
-import {redirect} from '@shopify/remix-oxygen';
+import {LoaderFunctionArgs, redirect} from '@shopify/remix-oxygen';
+import {useMultipassToken} from '~/lib/multipass';
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const isLoggedIn = await useMultipassToken(request);
+
+  // show log in screen if not logged in, otherwise redirect to account/orders
+  if (!isLoggedIn) {
+    return redirect('/account/login');
+  }
+
   return redirect('/account/orders');
 }
