188d9feb279ffb3481aca0aca4567f6c5136fe64
Zacarie Carr <zacariecarr@Zacaries-MacBook-Pro.local>
2024-07-31 10:02:07 +1000

    Updated account route redirects
    - Removed the context.customerAccount API
    - Updated redirect logic based on useMultipassToken

--- app/routes/($locale).account.$.tsx
+++ app/routes/($locale).account.$.tsx
@@ -1,8 +1,13 @@
 import {redirect, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import { useMultipassToken } from '~/lib/multipass';
 
 // fallback wild card for all unauthenticated routes in account section
-export async function loader({context}: LoaderFunctionArgs) {
-  await context.customerAccount.handleAuthStatus();
+export async function loader({context, request}: LoaderFunctionArgs) {
+  const isLoggedIn = await useMultipassToken(request);
+
+  if (!isLoggedIn) {
+    return redirect('/account/login');
+  }
 
   return redirect('/account');
 }
